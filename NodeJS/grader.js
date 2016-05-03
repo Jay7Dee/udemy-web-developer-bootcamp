23e40a58d714e4fa13607cd7b6c5fa007ec6b93b
Garrett Jeffrey <garrettjeffrey@users.noreply.github.com>
2016-05-02 21:41:02 -0400

    Update grader.js

--- NodeJS/grader.js
+++ NodeJS/grader.js
@@ -11,10 +11,10 @@ function average(scores){
     return Math.round(avg);
 }
 
-console.log("Average score for environment science");
+console.log("Average score for Comp Sci");
 var scores = [90, 98, 89, 100, 100, 86, 94];
 console.log(average(scores)); //should return 94
 
-console.log("Average score for organic chemistry");
+console.log("Average score for Comp Sci History");
 var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
 console.log(average(scores2)); //should return 68
