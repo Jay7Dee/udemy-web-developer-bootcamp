8b3e1b566952e94be8731abbe13eae7d5c442c7d
Garrett Jeffrey <garrettjeffrey@users.noreply.github.com>
2016-05-03 22:15:19 -0400

    Update listProducts.js

--- NodeJS/listProducts.js
+++ NodeJS/listProducts.js
@@ -11,3 +11,19 @@ function products(num){
 
 console.log("\n ================== \n WELCOME TO MY SHOP \n ==================");
 products(10);
+
+//Should print this below.
+
+ ================== 
+ WELCOME TO MY SHOP 
+ ==================
+Licensed Granite Fish - $919.00
+Rustic Granite Gloves - $318.00
+Handmade Metal Table - $366.00
+Small Soft Chips - $400.00
+Refined Granite Pants - $161.00
+Handmade Rubber Mouse - $431.00
+Sleek Steel Shoes - $64.00
+Practical Plastic Bike - $894.00
+Generic Fresh Table - $172.00
+Small Soft Towels - $669.00
