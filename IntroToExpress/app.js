b6d33e50e30ca41de235cdb990ac454da7337c71
Garrett Jeffrey <garrettjeffrey@users.noreply.github.com>
2016-05-04 21:50:17 -0400

    Update app.js

--- IntroToExpress/app.js
+++ IntroToExpress/app.js
@@ -1,1 +1,23 @@
+var express = require("express");
+var app = express();
 
+app.get("/", function(req, res){
+    res.send("Hi There!");
+});
+
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+
+app.get("/dog", function(req, res){
+    res.send("Meow!");
+})
+
+app.get("*", function(req, res){
+    res.send("You are a star!");
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started!");
+});
