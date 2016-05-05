e41b3fc5a761eb4e9fb0b795c0a290d9c027acaa
Garrett Jeffrey <garrettjeffrey@users.noreply.github.com>
2016-05-05 07:59:24 -0400

    Update app.js

--- IntroToExpress/app.js
+++ IntroToExpress/app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 
+
 app.get("/", function(req, res){
     res.send("Hi There!");
 });
@@ -13,11 +14,23 @@ app.get("/dog", function(req, res){
     res.send("Meow!");
 })
 
+app.get("/r/:subredditName", function(req, res){
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to the " + subreddit.toUpperCase() + " subreddit!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
+    console.log(req.params);
+    res.send("Welcome to the comments page!");
+});
+
+
 app.get("*", function(req, res){
     res.send("You are a star!");
 });
 
 
+//This is for talking with the c9.io servers
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
 });
